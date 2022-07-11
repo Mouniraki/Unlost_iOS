358e667259f78c1b3890a28840f669f9e199bc6a
Mounir Raki <mouniraki@protonmail.com>
2022-07-11 10:12:28 +0200

    Initial Commit

---
+++ Unlost/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unlost
+//
+//  Created by Mounir Raki on 11.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unlost/UnlostApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnlostApp.swift
+//  Unlost
+//
+//  Created by Mounir Raki on 11.07.22.
+//
+
+import SwiftUI
+
+@main
+struct UnlostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
